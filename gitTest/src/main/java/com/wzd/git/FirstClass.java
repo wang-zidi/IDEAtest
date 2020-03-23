74d1d16163a2c3a80635f4070c39568ec9691d0e
wang-zidi <272837906@qq.com>
2020-03-23 14:49:26 +0800

    修改FirstClass

--- gitTest/src/main/java/com/wzd/git/FirstClass.java
+++ gitTest/src/main/java/com/wzd/git/FirstClass.java
@@ -4,5 +4,10 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+    }
+
+    public void test(){
+        int i = 0;
     }
 }
